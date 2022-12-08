5a495677666c702131e725732e27dbf988abdd3c
tafesea <abaynehaddis@gmail.com>
2022-12-08 08:59:22 -0800

    Last check

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#include <stdio.h> 
-/*The main Return Puts Output*/
+#include <stdio.h>
+/**
+*The main Return Puts Outputii
+*to write a C programing with put
+*Like this
+*/
 int main(void)
 {
- puts( "\"Programming is like building a multilingual puzzle" );
- return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0); /* This is return value zero */
 }
