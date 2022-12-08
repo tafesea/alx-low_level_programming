840120436ae6dc84993ec6f27f309febbc898b3b
tafesea <abaynehaddis@gmail.com>
2022-12-08 11:20:21 -0800

    101-quote.c 

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-*main - Entry point
-*Return: Always 1 (Success)
+*main - Prints out the last part of a quote in the standard error.
+*Return: 1 if success.
 */
 int main(void)
 {
