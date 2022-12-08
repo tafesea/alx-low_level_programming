d0545d75d05cbcbd2acd1dd3a06e8dc996d29466
tafesea <abaynehaddis@gmail.com>
2022-12-08 09:50:18 -0800

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Description: 'Printing with printf Function'
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+printf("Programming is like building a multilingual puzzle\n");
+return (0);
+}
