7f6302821420f5dbe7007d361bea9ea47182c48a
tafesea <abaynehaddis@gmail.com>
2022-12-08 18:07:10 -0800

    self code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
 *main - Entry Point
@@ -5,7 +6,10 @@
 *Return: Always 1(Error)
 */
 int main(void)
+
 {
-write(2,  "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n");
+char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
+write(2,  err, 59);
 return (1);
 }
+
