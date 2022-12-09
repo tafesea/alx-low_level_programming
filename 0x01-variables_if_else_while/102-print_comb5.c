a4a8c6ea0163882e077d1acc5cec917163944c94
tafesea <abaynehaddis@gmail.com>
2022-12-09 13:07:13 -0800

    102-print_comb5.c12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
-*main - Entry poin
+*main - Entry point
 *Return: Always 0
 */
 
 int main(void)
 
 {
-
 int p, q;
 for (p = 0; p <= 98; p++)
 {
-
 for (q = p + 1; q <= 99; q++)
 
 {
@@ -26,13 +25,11 @@ if (p == 98 && q == 99)
 continue;
 putchar(',');
 putchar(' ');
-putchar('\n');
+}
 }
 
-}
 putchar('\n');
 
 return (0);
 
 }
-
