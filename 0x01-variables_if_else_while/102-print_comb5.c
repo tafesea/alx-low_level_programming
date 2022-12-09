e0509fc25790c61c21415458f998149849a996f5
tafesea <abaynehaddis@gmail.com>
2022-12-09 12:33:11 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,11 +7,15 @@
 */
 
 int main(void)
+
 {
+
 int p, q;
 for (p = 0; p <= 98; p++)
 {
+
 for (q = p + 1; q <= 99; q++)
+
 {
 putchar((p / 10) + '0');
 putchar((p % 10) + '0');
@@ -24,8 +28,11 @@ putchar(',');
 putchar(' ');
 putchar('\n');
 }
+
 }
 putchar('\n');
+
 return (0);
+
 }
 
