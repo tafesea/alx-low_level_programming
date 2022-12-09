b0b740a0e1da4947f15afa474986d4ed42743b61
tafesea <abaynehaddis@gmail.com>
2022-12-09 10:19:17 -0800

    Coomit Last Correction

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,23 +3,17 @@
 #include <time.h>
 
 /**
- *  * main - Entry point
- *   *
+*main - Entry point
 *Return: Always 0 (Success)
- */
+*/
 int main(void)
 
 {
-char low, e, q;
-e = 'e';
-q = 'q';
+char low;
 for (low = 'a'; low <= 'z'; low++)
-{
-if (low != e && low != q)
 putchar(low);
-
-}
-
+for (low = 'A'; low <= 'Z'; low++)
+putchar(low);
 putchar('\n');
 return (0);
 
