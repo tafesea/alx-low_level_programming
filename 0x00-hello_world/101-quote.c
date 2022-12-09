ef6e5a7d1e60621ad6aa1c80e03414c881cf0732
tafesea <abaynehaddis@gmail.com>
2022-12-08 17:56:41 -0800

    commit 101-quoete.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
 */
 int main(void)
 {
-char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
-write(2,  err, 59);
+write(2,  "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n");
 return (1);
 }
