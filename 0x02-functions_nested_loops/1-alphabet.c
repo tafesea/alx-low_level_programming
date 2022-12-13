f098de617e43220d6eeccb22bfaea54545df877b
tafesea <abaynehaddis@gmail.com>
2022-12-13 04:21:06 -0800

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
-*main - check the code
-*
-*Return: Always 0.
+*print_alphabet - Check description
+*Description: It prints the alphabet in lowercase fallowed by a new line
+*Return: Nothing.
 */
 
 void print_alphabet(void)
