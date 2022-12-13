1dd633d7cdd201a3e131125d215cc6bbf336c3f0
tafesea <abaynehaddis@gmail.com>
2022-12-13 11:46:50 -0800

    100 table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,21 +7,16 @@
 */
 void print_times_table(int n)
 {
-
 int i, j;
 if (n > 0 && n < 15)
 {
 for (i = 0; i <= n; i++)
 {
 _putchar('0');
-
 for (j = 1; j <= n; j++)
 putformat(i * j);
-
 _putchar('\n');
-
 }
-
 }
 }
 /**
@@ -32,14 +27,12 @@ _putchar('\n');
 */
 void putformat(int n)
 {
-
 if (n <= 9)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(' ');
-
 _putchar(n + '0');
 
 }
