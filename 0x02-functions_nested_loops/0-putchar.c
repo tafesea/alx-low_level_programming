3b1d3f09c2dc70a564c4c777014fc8a82ad5966e
tafesea <abaynehaddis@gmail.com>
2022-12-13 02:21:04 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+*Main Checkin the diiscription
+*Description: prints _putchar, followed by a new line.
+*Return 0.
+*/
+
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
